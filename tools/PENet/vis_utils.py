e7d41bffc16fbaf517ede6eb11b38ec976a9cee0
Leo <leo.schaefer4@yahoo.de>
2025-03-20 12:05:56 +0100

    PENet - save_depth_as_points updated to handle ImageSets not starting with frame 0

--- tools/PENet/vis_utils.py
+++ tools/PENet/vis_utils.py
@@ -130,45 +130,44 @@ def get_fov_flag(pts_rect, img_shape, calib):
 def save_depth_as_points(depth, idx, root_path): ##########
 
     
-    ########## File Index Preprocessing and Try Except Block added to use ImageSets that don't start with 000000 idx or contain  ##########
-    ###### OWN added feature  until next hashs ##########
-    # ectract the mode (e.g. 'testing' or 'training' from the path
-    # type_ImageSet = root_path.split('/')[-1]
-    # if type_ImageSet == 'testing':
-    #     # open the /workspace/data/kitti/ImageSets/test.txt file and get the index of the image
-    #     # the content is e.g. 
-    #     # with open('/workspace/data/kitti/ImageSets/test.txt', 'r') as f:
+    ########## File Index Preprocessing added to use ImageSets that don't start with 000000 idx or contain  ##########
+    #ectract the mode (e.g. 'testing' or 'training' from the path
+    type_ImageSet = root_path.split('/')[-1]
+    if type_ImageSet == 'testing':
+        # open the /workspace/data/kitti/ImageSets/test.txt file and get the index of the image
+        # the content is e.g. 
+        # with open('/workspace/data/kitti/ImageSets/test.txt', 'r') as f:
 
-    #     with open('/workspace/data/kitti/ImageSets/test.txt', 'r') as f: # for iw_custom_dataset2
-    #         lines = f.readlines()
-    #         file_idx = int(lines[idx].strip())
+        with open('/workspace/data/kitti/ImageSets/test.txt', 'r') as f: # for iw_custom_dataset2
+            lines = f.readlines()
+            file_idx = int(lines[idx].strip())
     
-    # elif type_ImageSet == 'training':
-    #     # if the index extends the ImageSet for training, then retrieve the index from the val.txt instead of train.txt
-    #     # with open('/workspace/data/kitti/ImageSets/train.txt', 'r') as f:
+    elif type_ImageSet == 'training':
+        # if the index extends the ImageSet for training, then retrieve the index from the val.txt instead of train.txt
+        # with open('/workspace/data/kitti/ImageSets/train.txt', 'r') as f:
 
-    #     with open('/workspace/data/kitti/ImageSets/train.txt', 'r') as f: # for iw_custom_dataset2
-    #         lines = f.readlines()
-    #         if idx <= len(lines):
-    #             file_idx = int(lines[idx].strip())
-    #         if idx > len(lines):
-    #             idx = idx - len(lines)      # reduce per number of lines to get right index in val.txt
+        with open('/workspace/data/kitti/ImageSets/train.txt', 'r') as f: # for iw_custom_dataset2
+            lines = f.readlines()
+            if idx <= len(lines):
+                file_idx = int(lines[idx].strip())
+            if idx > len(lines):
+                idx = idx - len(lines)      # reduce per number of lines to get right index in val.txt
 
-    #             with open('/workspace/data/kitti/ImageSets/val.txt', 'r') as f:
-    #                 lines = f.readlines()
-    #                 file_idx = int(lines[idx].strip())
+                with open('/workspace/data/kitti/ImageSets/val.txt', 'r') as f:
+                    lines = f.readlines()
+                    file_idx = int(lines[idx].strip())
     
-    # file_idx = str(file_idx).zfill(6)
-    ################
+    file_idx = str(file_idx).zfill(6)
+    ###############
 
-    file_idx = str(idx).zfill(6)
+    # lightweight option for the training pipeline
+    # file_idx = str(idx).zfill(6)
 
 
     file_image_path = os.path.join(root_path, 'image_2', file_idx + '.png')
     file_velo_path = os.path.join(root_path, 'velodyne', file_idx + '.bin')
     file_calib = os.path.join(root_path, 'calib', file_idx + '.txt')
 
-    # try:
     calib = calibration_kitti.Calibration(file_calib)
 
     lidar = np.fromfile(str(file_velo_path), dtype=np.float32).reshape(-1, 4)
@@ -187,29 +186,29 @@ def save_depth_as_points(depth, idx, root_path): ##########
     # save the lidar as .pcd file type 
     print("#+# Saving the raw_lidar_from_file as .npy & .pcd file")
 
-    np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_from_raw_file.npy', lidar)
-    save_point_cloud_as_pcd(lidar, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_from raw_file.pcd')
+    # np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_from_raw_file.npy', lidar)
+    # save_point_cloud_as_pcd(lidar, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_from raw_file.pcd')
 
     pts_rect = calib.lidar_to_rect(lidar[:, 0:3])
 
     #+# print the pts_rect to check if the points are in the right range (use current date time)
     print(f'BLOCK - Main Iterate in Loop {datetime.now().strftime("%Y%m%d_%H%M%S")}: pts_rect: {pts_rect}')
     print("#+# Saving the pts_rect as .npy & .pcd file")
-    np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_pts_rect_I.npy', pts_rect)
-    save_point_cloud_as_pcd(pts_rect, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_pts_rect_I.pcd')
+    # np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_pts_rect_I.npy', pts_rect)
+    # save_point_cloud_as_pcd(pts_rect, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_pts_rect_I.pcd')
 
     fov_flag = get_fov_flag(pts_rect, image.shape, calib)
     print(f'BLOCK - Main Iterate in Loop {datetime.now().strftime("%Y%m%d_%H%M%S")}: fov_flag: {fov_flag}')   
     print("#+# Saving the get_fov_flag as .npy & .pcd file")
-    np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_fov_flag_II.npy', fov_flag)
+    # np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_fov_flag_II.npy', fov_flag)
 
     #Commented out the FOV_flag #+#        ### To be tested again
     lidar = lidar[fov_flag]
     print(f'BLOCK - Main Iterate in Loop {datetime.now().strftime("%Y%m%d_%H%M%S")}: Applied the fov_flag to the lidar points')
     #+# save the lidar to check if the points are in the right range (use current date time)
     print("#+# Saving the lidar with applied fov as .npy & .pcd file")
-    np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_with_applied_fov_III.npy', lidar)
-    save_point_cloud_as_pcd(lidar, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_with_applied_fov_III.pcd')
+    # np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_with_applied_fov_III.npy', lidar)
+    # save_point_cloud_as_pcd(lidar, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_lidar_with_applied_fov_III.pcd')
     
 
     paths = os.path.join(root_path, 'velodyne_depth')
@@ -230,11 +229,8 @@ def save_depth_as_points(depth, idx, root_path): ##########
     #+# save the final points to the pipeline_investigation check 
     print("#+# Saving the final_points as .npy & .pcd file")
     np.save(f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_final_points_velodyne_depth_IV.npy', final_points)
-    save_point_cloud_as_pcd(final_points, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_final_points_velodyne_depth_IV.pcd')
+    # save_point_cloud_as_pcd(final_points, f'/workspace/data/kitti/training/pipeline_investigation/{datetime.now().strftime("%Y%m%d_%H%M%S")}_{file_idx}_final_points_velodyne_depth_IV.pcd')
 
-    # except Exception as e:
-    #     print("For image idx: ", file_idx, " Error: ", e)
-    #     pass
 
 
 def save_depth_as_uint16png_upload(img, filename):
