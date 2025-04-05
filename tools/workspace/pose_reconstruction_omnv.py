8e1c3b26c9c8c252f5ff2e3db845600cfb4d8a39
Leo <leo.schaefer4@yahoo.de>
2025-04-05 18:54:45 +0000

    Add pose_reconstruction_omnv to get omniverse world coordinate pose for bboxes

---
+++ tools/workspace/pose_reconstruction_omnv.py
@@ -0,0 +1,125 @@
+import json
+import numpy as np
+from scipy.spatial.transform import Rotation as R
+
+def get_camera_pose_omnv_world(kitti_cam_pose_omnv_world_path):
+    # get kitti camera pose (not omniverse default camera convention) in omniverse world coordinate frame
+    with open(kitti_cam_pose_omnv_world_path) as f:
+        kitti_cam_poses_omnv_world = json.load(f)
+
+    kitti_cam_positions = kitti_cam_poses_omnv_world['camera_positions']
+    kitti_cam_rotations = kitti_cam_poses_omnv_world['camera_rotations']
+
+    cam_transform_matrices = []
+
+    for frame in range(len(kitti_cam_positions)):
+        position = kitti_cam_positions[frame]
+        rotation_deg = kitti_cam_rotations[frame]  # [pitch, yaw, roll] in degrees
+
+        # Convert from degree to radian: Assumed order = roll, pitch, yaw (== xyz order)
+        rotation = R.from_euler('xyz', np.radians(rotation_deg), degrees=False)
+        rot_matrix = rotation.as_matrix()
+
+        # Create homogeneous transformation matrix
+        T = np.eye(4)
+        T[:3, :3] = rot_matrix
+        T[:3, 3] = position
+
+        frame_id = f"{frame:06d}"
+
+        cam_transform_matrices.append({
+            "Tr_kitti_cam_to_omni_world": T, 
+            "frame_id": frame_id
+        })    
+
+        print(f"Frame {frame_id} - Camera Transformation Matrix:\n{T}\n")
+
+    return cam_transform_matrices
+
+def get_bbox_pose_kitti_cam(pred_bbox_pose_kitti_cam_path):    
+    # load data from csv file
+    # content: name, truncated, occluded, alpha, u1, v1, u2, v2, h, w, l, x_cam, y_cam, z_cam, rotation_y, score
+   
+    # Output:
+        # BBox pose in the camera coordinate frame as format to multiply with transform matrix
+
+    with open(pred_bbox_pose_kitti_cam_path, 'r') as f:
+        bbox_lines_with_header = f.readlines()
+        bbox_lines = [line.strip().split(',') for line in bbox_lines_with_header[1:]]  # Skip the header
+
+    # Extract the bbbox_poses in the camera coordinate frame
+    pred_bboxes_poses_kitti_cam = []
+    
+    for bbox in bbox_lines:
+        name = bbox[0]
+        x, y, z = float(bbox[11]), float(bbox[12]), float(bbox[13])
+        rotation_y = float(bbox[14]) # yaw in radians (KITTI format)
+        rot_matrix = R.from_euler('y', rotation_y, degrees=False).as_matrix()
+        
+        # Create a 4x4 transformation matrix [R| t]
+        T = np.eye(4)
+        T[:3, :3] = rot_matrix
+        T[:3, 3] = [x, y, z]
+
+        pred_bboxes_poses_kitti_cam.append({
+            'name': name,
+            'pred_bbox_pose_kitti_cam': T
+            })
+
+        print(f"Bbox pose: \n{T}")
+
+
+    return pred_bboxes_poses_kitti_cam
+
+def reconstruct_bbox_pose_omnv_world(cam_transform_matrices, pred_bboxes_poses_kitti_cam, current_frame_id):
+    # Transforms each predicted bounding box pose PER FRAME from camera coordinates to Omniverse world coordinates.
+
+    bbox_poses_omnv_world = []
+
+    for frame in cam_transform_matrices:
+        if frame['frame_id'] == current_frame_id:
+            # Get the cam_T for the current frame 
+            cam_T = frame['Tr_kitti_cam_to_omni_world']
+
+            for i in range(len(pred_bboxes_poses_kitti_cam)):     # BBOXES must be available for specific frame
+                bbox_T = pred_bboxes_poses_kitti_cam[i]['pred_bbox_pose_kitti_cam']
+
+                bbox_world = cam_T @ bbox_T
+
+                bbox_poses_omnv_world.append(bbox_world)
+
+                # Print the transformed bounding box poses
+                print(f"Bbox pose in omniverse world coordinates: \n{bbox_world}")
+
+    return bbox_poses_omnv_world
+
+
+
+
+
+
+if __name__ == "__main__":
+    # load kitti camera pose in omniverse world coordinate frame from json file 
+    # Path:'/workspace/tools/workspace/camera_poses.json'
+
+    kitti_cam_pose_omnv_world_path = '/workspace/tools/workspace/camera_poses.json'
+    pred_bbox_pose_kitti_cam_path = '/workspace/tools/workspace/mocked_pred_boxes.csv'
+
+    cam_transform_matrices = get_camera_pose_omnv_world(kitti_cam_pose_omnv_world_path)
+
+
+    ####### Currently mocked data and only for one frame #######
+    pred_bbox_pose_kitti_cam = get_bbox_pose_kitti_cam(pred_bbox_pose_kitti_cam_path)
+
+    # frame id: format = 000000
+    mocked_frame_ids = ['000000', '000001', '000002', '000003']
+
+    bbox_poses_omnv_world_for_all_frames = []
+
+    # Iterate over the mocked frame ids and reconstruct the bounding box poses
+    for frame in range(len(mocked_frame_ids)):
+        current_frame_id = mocked_frame_ids[frame]
+        bbox_pose_omnv_world = reconstruct_bbox_pose_omnv_world(cam_transform_matrices, pred_bbox_pose_kitti_cam, current_frame_id)
+        bbox_poses_omnv_world_for_all_frames.append(bbox_pose_omnv_world)
+        
+        print(f"Frame {mocked_frame_ids[frame]} - Bbox poses in omniverse world coordinates: \n{bbox_pose_omnv_world}\n")
