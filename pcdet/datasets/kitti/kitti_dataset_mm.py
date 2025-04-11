2f7c546c2983960343012d156d3bde0992d21741
Leo Schaefer <leo.schaefer4@yahoo.de>
2025-04-11 14:23:12 +0200

    Update latest DS8 cfgs from Exc

--- pcdet/datasets/kitti/kitti_dataset_mm.py
+++ pcdet/datasets/kitti/kitti_dataset_mm.py
@@ -447,7 +447,7 @@ class KittiDatasetMM(DatasetTemplate):
             if self.training and 'num_points_in_gt' in annos:
                 
                 ######### WARNUNG #########################################################################
-                print('WARNING: num_points_in_gt is USED in the current implementation')
+                # print('WARNING: num_points_in_gt is USED in the current implementation')
                 nmask = annos['num_points_in_gt']>0
                 # print('WARNING: num_points_in_gt is NOT used in the current implementation')
                 # nmask = annos['num_points_in_gt']>=0
